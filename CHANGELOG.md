342cdefa460d55f4b4b5ddd216de284c218af01f
n.anisimova <nadia11fadeeva@gmail.com>
2018-11-11 13:05:07 +0700

    setup enviroment

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Travel checklists
+
+### 0.0.1
+- Interface for choosing checklist depend on travel parameters
+- Buttons to add new checklist
+- Interactive checklist
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'app'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin('app', {} ),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new WebpackMd5Hash()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'app'),
+        compress: false,
+        port: 9000
+    }
+};
